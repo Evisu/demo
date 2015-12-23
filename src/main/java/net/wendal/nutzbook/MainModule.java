d042afb8d1ecf9da3535ab15e2c8b1add4f3a5ab
Evisu <sev7n.v@gmail.com>
2015-12-23 10:09:25 +0800

    nutz demo

---
+++ src/main/java/net/wendal/nutzbook/MainModule.java
@@ -0,0 +1,24 @@
+package net.wendal.nutzbook;
+
+import org.nutz.mvc.annotation.ChainBy;
+import org.nutz.mvc.annotation.IocBy;
+import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.SetupBy;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
+
+/**
+ * 添加模块类自动扫描
+ * 
+ * @author walkman
+ *
+ */
+@SetupBy(value=MainSetup.class)
+@IocBy(type=ComboIocProvider.class, args={"*js", "ioc/",
+        "*anno", "net.wendal.nutzbook",
+        "*tx",
+        "*org.nutz.integration.quartz.QuartzIocLoader"})
+@Modules(scanPackage = true)
+@ChainBy(args="mvc/nutzbook-mvc-chain.js")
+public class MainModule {
+
+}
